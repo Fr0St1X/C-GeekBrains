9610097300a2a76c53eef311ce4634b2a74c40da
Frostix <fr0st1x@outlook.com>
2026-03-29 18:47:15 +0300

    Example002_HelloUzver

---
+++ Example002_HelloUzver/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Как тебя зовут, моя сладкая булочка?");
+Console.Write("Имя вводить тут: ");
+string username = Console.ReadLine()!;
+Console.Write("Проснись, ");
+Console.Write(username);
+Console.Write("! Ты обосрался!");
